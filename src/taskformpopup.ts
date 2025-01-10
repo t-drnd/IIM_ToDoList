01f468317b97d1f9ae92646e1d24be9c82b93747
Mathis Dardé <dardemathis@gmail.com>
2025-01-10 12:53:08 +0100

    feat: création de la logique d'affichage du popup

---
+++ src/taskformpopup.ts
@@ -0,0 +1,28 @@
+function openTaskPopup(): void {
+  document.addEventListener("DOMContentLoaded", () => {
+    const openPopupButton = document.getElementById("addtaskbutton");
+    const popup = document.getElementById("addtaskpopup");
+    const overlay = document.getElementById("popupoverlay");
+    const closeButton = document.querySelector(".closepopup");
+    const form = popup?.querySelector("form") as HTMLFormElement;
+
+    if (!openPopupButton || !popup || !overlay || !closeButton || !form) return;
+
+    openPopupButton.addEventListener("click", () => {
+      popup.style.display = "block";
+      overlay.style.display = "block";
+    });
+
+    closeButton.addEventListener("click", closePopup);
+
+    overlay.addEventListener("click", closePopup);
+
+    function closePopup() {
+      popup.style.display = "none";
+      overlay.style.display = "none";
+      form.reset();
+    }
+  });
+}
+
+openTaskPopup();
